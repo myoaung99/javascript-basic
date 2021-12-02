3abc8d519ca1553be394d64f87fcb38a91868a1e
MyoMyintAung <myomyintaung064@gmail.com>
2021-12-02 22:03:54 +0630

    String Methods

---
+++ string.js
@@ -0,0 +1,20 @@
+const msg = "hey leave me alone. i dont want to see you plz.";
+
+//to uppercase method
+const angryMsg = msg.toUpperCase();
+
+//slice the string
+const greeting = msg.slice(0, 3);
+
+//string's only property LENGTH
+const len = msg.length
+
+msg.charAt(3);
+msg.indexOf('m');
+
+
+
+console.log(angryMsg);
+console.log(greeting);
+console.log(len);
+
