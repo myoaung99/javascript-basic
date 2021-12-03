3196479bb938369de32ba0e707fb3d543b82856f
MyoMyintAung <myomyintaung064@gmail.com>
2021-12-03 21:58:58 +0630

    Javascript Basic

--- string.js
+++ string.js
@@ -46,3 +46,14 @@ else if(num >= 0.5){
     console.log(num);
 }
 
+const age = Math.floor(Math.random() * 65 ) + 1;
+console.log(age);
+if (age < 5){
+    console.log("You are a baby. You get in for free");
+} else if(age < 10){
+    console.log("You are a child. You pay $10.");
+} else if (age < 65){
+    console.log("You are a adult. You pay $20");
+} else if(age >= 65){
+    console.log("You are a senior. You pay $10.");
+}
