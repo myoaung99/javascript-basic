a7cfb058e76ada0f7d602f9d7fec779867c86ad7
MyoMyintAung <myomyintaung064@gmail.com>
2021-12-09 11:24:27 +0630

    Javascript Basic

--- string.js
+++ string.js
@@ -296,13 +296,94 @@
 
 //Methods
 
-let myObj = {
-    "name": "dev.myint",
-    "drink": function(){
-        console.log("Drinking water.")
-    },
-    eat(){
-        console.log("Eating rice.")
-    }
-}
+// let myObj = {
+//     "name": "dev.myint",
+//     "drink": function(){
+//         console.log("Drinking water.")
+//     },
+//     eat(){
+//         console.log("Eating rice.")
+//     }
+// }
 
+// let square = {
+//     area(side){
+//         return side * side;
+//     },
+//     perimeter(side){
+//         return side * 4;
+//     }
+// }
+
+// let hen = {
+//     "name": "Helen",
+//     "eggCount": 0,
+//     "layAnEgg": () => {
+//         this.eggCount++;
+//     }
+// }
+
+// const greet = (str) => {
+//     console.log(`Hey, ${str}`);
+// }
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const oddNumXtwo = numbers.filter(n => n % 2 === 1).map(n => n * 2);
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99,
+        year: 1984
+    },
+    {
+        title: 'Sharknado',
+        score: 35,
+        year: 2013
+    },
+    {
+        title: '13 Going On 30',
+        score: 70,
+        year: 2004
+    },
+    {
+        title: 'Stand By Me',
+        score: 85,
+        year: 1986
+    },
+    {
+        title: 'Waterworld',
+        score: 62,
+        year: 1995
+    },
+    {
+        title: 'Jingle All The Way',
+        score: 71,
+        year: 1996
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 2019
+    },
+    {
+        title: 'Notting Hill',
+        score: 77,
+        year: 1999
+    },
+    {
+        title: 'Alien',
+        score: 90,
+        year: 1979
+    }
+]
+
+const goodMovies = movies.filter(m => m.score > 80);
+const badMovies = movies.filter(m => m.score < 70);
+
+const goodTitles = goodMovies.map(m => m.title);
+
+const goodMovieTitle = movies.filter(m => m.score > 80).map(m => m.title);
+
+const movie = movies.forEach(m => console.log(m.title.toUpperCase()))
